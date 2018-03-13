ee57682238fb0d3aa65cc522409520b3d55808d8
João Paulo <jpbelentani@Hotmail.com>
2018-03-13 18:25:50 -0300

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Random randomNumberGenerator = new Random();
-                int number = randomNumberGenerator.nextInt(4);
+                int number = randomNumberGenerator.nextInt(5);
 
                 ballDisplay.setImageResource(ballArray[number]);
             }
